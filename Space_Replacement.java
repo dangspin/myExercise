afe41a66687e7911223bcf74c34c973e3325e440
Charlie Dang <dangxiaoqian@gmail.com>
2017-04-17 21:29:47 -0500

    Create Space_Replacement.java

---
+++ Space_Replacement.java
@@ -0,0 +1,36 @@
+public class Solution {
+    /**
+     * @param string: An array of Char
+     * @param length: The true length of the string
+     * @return: The true length of new string
+     */
+    public int replaceBlank(char[] string, int length) {
+        // Write your code here
+        if (length==0){return 0;}
+        int count=0;
+        for (int i=0;i<length;i++){
+            if (string[i]==' '){
+                count++;
+            }
+        }
+        
+        int newLen = length + count*2;
+        string[newLen] = 0;
+        int j = 1;
+        for(int i=length-1;i>=0;i--){
+            if(string[i] != ' '){
+                string[newLen - j] = string[i];
+                j++;
+            }
+            else{
+                string[newLen - j] = '0';
+                j++;
+                string[newLen - j] = '2';
+                j++;
+                string[newLen - j] = '%';
+                j++; 
+            }
+        }
+        return newLen;
+    }
+}
