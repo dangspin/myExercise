a0501a2bfe7c1cf184b92689a9387773a7cbd051
Charlie Dang <dangxiaoqian@gmail.com>
2017-04-19 17:13:52 -0500

    Create Insert_Interval.java

---
+++ Insert_Interval.java
@@ -0,0 +1,42 @@
+/**
+ * Definition of Interval:
+ * public classs Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ */
+
+class Solution {
+    /**
+     * Insert newInterval into intervals.
+     * @param intervals: Sorted interval list.
+     * @param newInterval: A new interval.
+     * @return: A new sorted interval list.
+     */
+    public ArrayList<Interval> insert(ArrayList<Interval> intervals, Interval newInterval) {
+        if (newInterval == null || intervals == null) {
+            return intervals;
+        }
+
+        ArrayList<Interval> results = new ArrayList<Interval>();
+        int insertPos = 0;
+
+        for (Interval interval : intervals) {
+            if (interval.end < newInterval.start) {
+                results.add(interval);
+                insertPos++;
+            } else if (interval.start > newInterval.end) {
+                results.add(interval);
+            } else {
+                newInterval.start = Math.min(interval.start, newInterval.start);
+                newInterval.end = Math.max(interval.end, newInterval.end);
+            }
+        }
+
+        results.add(insertPos, newInterval);
+
+        return results;
+    }
+}
