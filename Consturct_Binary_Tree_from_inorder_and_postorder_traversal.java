b0c46c3a1db38beb634ddcbb5f2bb349efa6022b
Charlie Dang <dangxiaoqian@gmail.com>
2018-01-23 16:28:29 +0800

    Update Consturct_Binary_Tree_from_inorder_and_postorder_traversal.java

--- Consturct_Binary_Tree_from_inorder_and_postorder_traversal.java
+++ Consturct_Binary_Tree_from_inorder_and_postorder_traversal.java
@@ -37,4 +37,38 @@ public class Solution {
         
         return root;
     }
+    
+    
+    // 2. Another approach: without extra space, but needs a help function and extra time.
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        // write your code here
+        if (inorder.length != postorder.length) {return null;}
+        
+        return myBuildTree(inorder, 0, inorder.length-1, postorder, 0, postorder.length-1);
+    }
+    
+    public int findPosition(int[] inorder, int instart, int inend, int key) {
+        for (int i = instart; i <= inend; i++) {
+            if (inorder[i] == key) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public TreeNode myBuildTree(int[] inorder, int instart, int inend, int[] postorder, int postart, int posend) {
+        if (instart > inend) {
+            return null;
+        }
+        
+        TreeNode root = new TreeNode(postorder[posend]);
+        
+        int position = findPosition(inorder, instart, inend, postorder[posend]);
+        
+        root.left = myBuildTree(inorder, instart, position-1, postorder, postart, postart + position- instart -1);
+        root.right = myBuildTree(inorder, position+1, inend, postorder, postart + position- instart, posend -1);
+        
+        return root;
+    }
 }
