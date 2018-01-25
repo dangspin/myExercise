816a182640ce41f9931d322e82bb8079aa0aef17
Charlie Dang <dangxiaoqian@gmail.com>
2018-01-25 07:00:25 -0600

    Update Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java

--- Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
+++ Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal.java
@@ -40,5 +40,35 @@ public class Solution {
         return root;
     }
     
+    //#2. Second version of solution
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        // write your code here
+        if (preorder.length != inorder.length) {return null;}
+        return myBuildTree(preorder, 0, preorder.length-1, inorder, 0, inorder.length-1);
+    }
+    
+    public TreeNode myBuildTree(int[] preorder, int prestart, int preend, int[] inorder, int instart, int inend) {
+        if (instart > inend) {
+            return null;
+        }
+        
+        TreeNode root = new TreeNode(preorder[prestart]);
+        int position = findPosition(inorder, instart, inend, preorder[prestart]);
+        
+        root.left = myBuildTree(preorder, prestart + 1, prestart + position - instart, inorder, instart, position - 1);
+        root.right = myBuildTree(preorder, position - inend + preend + 1, preend, inorder, position + 1, inend);
+        
+        return root;
+    }
+    
+    public int findPosition(int[] inorder, int instart, int inend, int key) {
+        for (int i = instart; i <= inend; i++) {
+            if (inorder[i] == key) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
 
 }
