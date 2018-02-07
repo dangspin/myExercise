a047f71203c8701e26b4f351eab39377a46cedf4
Charlie Dang <dangxiaoqian@gmail.com>
2018-02-07 13:33:10 +0800

    Update Maximum_Subarray.java

--- Maximum_Subarray.java
+++ Maximum_Subarray.java
@@ -17,4 +17,18 @@ public class Solution {
         
         return result;
     }
+    
+    public int maxSubArray(int[] nums) {
+        
+        // This is the DP version solution
+        int[] dp = new int[nums.length];
+		
+		dp[0] = nums[0];
+		for (int i = 1; i<nums.length; i++) {
+			dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
+		}
+		
+		Arrays.sort(dp);
+		return dp[nums.length-1];
+    }
 }
