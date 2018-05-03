f24d10f2b739896ab14274a1873c5b758097b45e
Charlie Dang <dangxiaoqian@gmail.com>
2018-05-03 14:47:18 +0800

    Update Binary_Tree_Level_Order_Traversal_II.java

--- Binary_Tree_Level_Order_Traversal_II.java
+++ Binary_Tree_Level_Order_Traversal_II.java
@@ -42,4 +42,23 @@ class Solution {
         
         return res;
     }
+    
+    // DFS version
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        helper(root, res, 0);
+        return res;
+    }
+    
+    public void helper(TreeNode root, List<List<Integer>> res, int level) {
+        if (root == null) {return ; }
+        if (level >= res.size()) {
+            res.add(0,new ArrayList<Integer>());
+        }
+        
+        helper(root.left, res, level + 1);
+        helper(root.right, res, level + 1);
+        
+        res.get(res.size()-level-1).add(root.val);
+    }
 }
