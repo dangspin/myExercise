38b586d251ba06b0dbae2194d4153a022b74a0e4
Charlie Dang <dangxiaoqian@gmail.com>
2018-05-05 14:53:29 +0800

    Create Integer_Break.java

---
+++ Integer_Break.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int integerBreak(int n) {
+        
+        // Not a difficult problem, very straightforward thinking, but this is about O(n^2).
+        // Just one thing need to be careful that the transition condition includes the comparison between intBreak[k].
+        // Because the inner loop updates this value everytime, so we need to check it to make sure it reaches the global maximum.
+        
+        int[] intBreak = new int[n+1];
+        
+        intBreak[1] = 1;
+        intBreak[2] = 1;
+        
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                intBreak[i] = Math.max(intBreak[i], Math.max(j*(i-j), j*intBreak[i-j]));
+            }
+        }
+        
+        return intBreak[n];
+    }
+}
